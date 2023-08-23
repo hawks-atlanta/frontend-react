22a152c703d1f07f3e4f4dcfbf181b1dbf7878d3
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-08-23 13:20:40 -0500

    chore(setup): Add sample unit test with `vitest` + `react-testing-library`

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,12 @@
+interface ButtonProps {
+  text: string;
+  type: 'anchor' | 'button';
+}
+
+export function Button({ text, type }: ButtonProps) {
+  if (type === 'anchor') {
+    return <a href="#">{text}</a>;
+  }
+
+  return <button type="button">{text}</button>;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  test: {
+    include: ['src/**/*.{test,spec}.{ts,tsx,js,jsx}'],
+    globals: false, 
+    environment: 'jsdom'
+  }
 });
