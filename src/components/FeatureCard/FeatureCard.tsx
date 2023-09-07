25472ba2882f46429e6a882ad40aaa2742a7cae0
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-09-07 17:48:38 -0500

    refactor: Remove some `div` and update some class-names

--- src/components/FeatureCard/FeatureCard.tsx
+++ src/components/FeatureCard/FeatureCard.tsx
@@ -6,12 +6,10 @@ interface FeatureCardProps {
 
 export function FeatureCard({ icon, title, description }: FeatureCardProps) {
   return (
-    <div className="m-2 rounded-lg bg-gray-100 p-10">
-      <div className="mx-1 flex flex-col items-center p-2 text-center sm:mx-2">
-        {icon}
-        <h3 className="mb-4 mt-2 text-2xl font-bold text-blue-600">{title}</h3>
-        <p className="text-lg text-black">{description}</p>
-      </div>
+    <div className="flex flex-col items-center justify-center gap-4 rounded-lg bg-gray-100 p-10 text-center shadow-sm">
+      {icon}
+      <h3 className="text-3xl font-bold text-blue-600">{title}</h3>
+      <p className="text-lg text-black">{description}</p>
     </div>
   );
 }
--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -6,45 +6,39 @@ import { FileDown, Files, FolderKey } from "lucide-react";
 export function App() {
   return (
     <main>
-      <div className="min-h-1/2 bg-blue-600 py-8">
+      <header className="min-h-1/2 bg-blue-600 pb-12 pt-8">
         <div className="mx-auto max-w-screen-2xl px-4">
-          <div className="max-w-md font-bold text-white">
-            <h1 className=" text-5xl">Access your files everywhere</h1>
-            <div className="mt-14 max-w-md">
-              <h1 className=" text-2xl">
-                Effortlessly store, organize and access your files with built-in
-                redundancy.
-              </h1>
-            </div>
-            <div className="mt-16">
-              <Button text="Create an Account" to="/register" />
-            </div>
+          <div className="flex max-w-md flex-col items-start gap-8 font-bold text-white">
+            <h1 className="text-5xl">Access your files everywhere</h1>
+            <p className="text-2xl">
+              Effortlessly store, organize and access your files with built-in
+              redundancy.
+            </p>
+            <Button text="Create an Account" to="/register" />
           </div>
         </div>
-      </div>
+      </header>
 
-      <section className=" px-4">
-        <div className="mx-auto max-w-screen-2xl">
-          <div className="mt-10 w-full text-center">
-            <h2 className="text-2xl font-bold text-blue-600">MAIN FEATURES</h2>
-          </div>
-          <div className="left-0 top-1/3 mt-14 flex w-full flex-col items-center justify-center sm:flex-row">
-            <FeatureCard
-              icon={<FolderKey size={52} />}
-              title="File key"
-              description="Keep your documents safe and secure in our distributed storage system."
-            />
-            <FeatureCard
-              icon={<FileDown size={52} />}
-              title="Save your files"
-              description="Store your files using our cloud service and download them from any other device."
-            />
-            <FeatureCard
-              icon={<Files size={52} />}
-              title="Manage your documents"
-              description="Use our system to organize and manage your files. Access them at any time."
-            />
-          </div>
+      <section className="mx-auto mt-10 max-w-screen-2xl px-4">
+        <h2 className="text-center text-4xl font-bold text-blue-600">
+          MAIN FEATURES
+        </h2>
+        <div className="mt-10 flex flex-col items-stretch justify-center gap-8 md:flex-row">
+          <FeatureCard
+            icon={<FolderKey size={52} />}
+            title="File key"
+            description="Keep your documents safe and secure in our distributed storage system."
+          />
+          <FeatureCard
+            icon={<FileDown size={52} />}
+            title="Save your files"
+            description="Store your files using our cloud service and download them from any other device."
+          />
+          <FeatureCard
+            icon={<Files size={52} />}
+            title="Manage your documents"
+            description="Use our system to organize and manage your files. Access them at any time."
+          />
         </div>
       </section>
     </main>
