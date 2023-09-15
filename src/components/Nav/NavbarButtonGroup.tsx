2ba54ec4e32e2d5b1f3ec175bf302b1fb1d3fc2c
Miguel Mateo Mendoza Rojas <115849391+MiguelMRojas@users.noreply.github.com>
2023-09-15 17:50:15 -0500

    style: horizontal margin

--- src/components/Nav/NavbarButtonGroup.tsx
+++ src/components/Nav/NavbarButtonGroup.tsx
@@ -10,7 +10,7 @@ export function NavbarIcon({ text, to }: NavbarButtonGroup) {
     <div className="flex items-center">
       <Link
         to={to}
-        className="rounded-lg border border-blue-600 bg-white p-2 text-lg font-bold text-blue-600 "
+        className="mx-0 rounded-lg border border-blue-600 bg-white p-2 text-lg font-bold text-blue-600"
       >
         {text}
       </Link>
