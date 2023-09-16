100aeb3f61b82d637f8ef4ae43ad944067f8ddf8
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-09-16 06:32:42 -0500

    test: Update tests selectors

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -5,19 +5,19 @@ describe("Button tests", () => {
 
   it("Should render an anchor with the given text and redirect to Login page", () => {
     cy.visit("/");
-    cy.contains("a", "Login").click();
+    cy.contains("Login").click();
     cy.url().should("include", "/login");
   });
 
   it("Should render an anchor with the given text and redirect to Register page", () => {
     cy.visit("/");
-    cy.contains("a", "Register").click();
+    cy.contains("Register").click();
     cy.url().should("include", "/register");
   });
 
   it("Should render an anchor with the given text and redirect to Create An Account page", () => {
     cy.visit("/");
-    cy.contains("a", "Create an Account").click();
+    cy.contains("Create an Account").click();
     cy.url().should("include", "/register");
   });
 });
--- cypress/e2e/navbar.cy.ts
+++ cypress/e2e/navbar.cy.ts
@@ -1,30 +1,33 @@
 describe("Navbar Test", () => {
   beforeEach(() => {
     cy.viewport("iphone-6");
+    cy.visit("/");
   });
 
+  it("Mobile menu should be hidden by default", () => {
+    cy.contains("Login").should("not.be.visible");
+    cy.contains("Register").should("not.be.visible");
+  })
+
   it("toggle menu when the menu button is clicked", () => {
-    cy.visit("/");
-    cy.get(".md\\:hidden").should("not.be.hidden");
     cy.get('button[aria-label="Open Menu"]').click();
-    cy.get(".md\\:hidden").should("be.visible");  
+    cy.contains("Login").should("be.visible");
+    cy.contains("Register").should("be.visible");
+
     cy.get('button[aria-label="Close Menu"]').click();
-    cy.get(".md\\:hidden").should("not.be.hidden");
+    cy.contains("Login").should("not.be.visible");
+    cy.contains("Register").should("not.be.visible");
   });
   
   
   it("should navigate to the Login page when Login is clicked", () => {
-    cy.visit("/");
     cy.get('button[aria-label="Open Menu"]').click();
-    cy.get(".md\\:hidden").should("be.visible");
     cy.contains("a", "Login").click();
     cy.url().should("include", "/login");
   });
 
   it("should navigate to the Register page when Register is clicked", () => {
-    cy.visit("/");
     cy.get('button[aria-label="Open Menu"]').click();
-    cy.get(".md\\:hidden").should("be.visible");
     cy.contains("a", "Register").click();
     cy.url().should("include", "/register");
   });
