5b9c8114cb7e9110f940b921846d96eb3a961510
Miguel Mateo Mendoza Rojas <115849391+MiguelMRojas@users.noreply.github.com>
2023-09-15 21:44:30 -0500

    fix: navbar test setup

--- cypress/e2e/navbar.cy.ts
+++ cypress/e2e/navbar.cy.ts
@@ -5,17 +5,17 @@ describe("Navbar Test", () => {
 
   it("toggle menu when the menu button is clicked", () => {
     cy.visit("/");
-    cy.get('button[aria-label="Open menu"]').click(); 
-    cy.get(".md\\:hidden").should("be.visible");
-    cy.get('button[aria-label="Open menu"]').click(); 
+    cy.get(".md\\:hidden").should("not.be.hidden");
+    cy.get('button[aria-label="Open Menu"]').click();
+    cy.get(".md\\:hidden").should("be.visible");  
+    cy.get('button[aria-label="Close Menu"]').click();
     cy.get(".md\\:hidden").should("not.be.hidden");
   });
   
   
-  
   it("should navigate to the Login page when Login is clicked", () => {
     cy.visit("/");
-    cy.get('button[aria-label="Open menu"]').click(); 
+    cy.get('button[aria-label="Open Menu"]').click();
     cy.get(".md\\:hidden").should("be.visible");
     cy.contains("a", "Login").click();
     cy.url().should("include", "/login");
@@ -23,9 +23,13 @@ describe("Navbar Test", () => {
 
   it("should navigate to the Register page when Register is clicked", () => {
     cy.visit("/");
-    cy.get('button[aria-label="Open menu"]').click(); 
+    cy.get('button[aria-label="Open Menu"]').click();
     cy.get(".md\\:hidden").should("be.visible");
     cy.contains("a", "Register").click();
     cy.url().should("include", "/register");
   });
 });
+
+
+
+
