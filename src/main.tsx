719a0d7ea6c5bdddcfed24ff4263a279a8f166af
Daniel Patiño <daniel.patino23@outlook.com>
2023-09-25 22:06:37 -0500

    fix: 'NavbarScreen' is defined but never used

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { App } from "./screens/App.tsx";
 import "./global.css";
-import { NavbarScreen } from "./components/Nav/Nav.tsx";
+// import { NavbarScreen } from "./components/Nav/Nav.tsx";
 import { Login } from "./screens/Login.tsx";
 import { Register } from "./screens/Register.tsx";
 import { Homepage } from "./screens/Home.tsx";
