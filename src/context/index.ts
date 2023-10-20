cd5774aaa01558d6853b5413e4914bdc74903230
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-10-20 13:17:18 -0500

    refactor: Update files view to use the files context

---
+++ src/context/index.ts
@@ -0,0 +1,2 @@
+export { AuthContext, AuthContextProvider } from "./AuthContext";
+export { FilesContext, FilesContextProvider } from "./FilesContext";
--- src/screens/FilePage.tsx
+++ src/screens/FilePage.tsx
@@ -1,54 +1,15 @@
 import { Sidebar } from "../components/Sidebar/Sidebar";
-import { listFilesService } from "../services/files/list-file.service";
-import { useContext, useEffect, useState } from "react";
-import { File } from "../types/entities";
 import { FileElement } from "../components/FileElement/FileCard";
-import { AuthContext } from "../context/AuthContext";
-import { useSearchParams } from "react-router-dom";
-import { toast } from "react-hot-toast";
-import { useNavigate } from "react-router-dom";
+import { FilesContext } from "../context/index";
+import { useContext } from "react";
 
 export function FilePage() {
-  // Url state
-  const [searchParams, _setSearchParams] = useSearchParams();
-  const directory = searchParams.get("directory");
-  const navigate = useNavigate();
-
-  const { session } = useContext(AuthContext);
-
-  // Files state
-  const [isLoading, setIsLoading] = useState(true);
-  const [files, setFiles] = useState<File[]>([]);
-
-  const fetchFiles = async () => {
-    setIsLoading(true);
-    const response = await listFilesService({
-      token: session?.token || "",
-      directory: directory
-    });
-
-    if (response.success) {
-      setFiles(response.files);
-    } else {
-      toast.error(response.msg);
-      navigate("/");
-    }
-
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    fetchFiles();
-  }, [directory]);
-
-  const addNewDirectory = (dir: File) => {
-    setFiles([...files, dir]);
-  };
+  const { areFilesLoading: isLoading, files } = useContext(FilesContext);
 
   return (
     <div className="flex h-[calc(100vh-5rem)]">
       <div className="w-1/5 bg-gray-200">
-        <Sidebar addFolderCallback={addNewDirectory} />
+        <Sidebar />
       </div>
       <div className="mx-6 flex w-4/5 flex-col overflow-y-auto bg-white">
         <div className="p-2">
