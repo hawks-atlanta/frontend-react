12cad214eac3404b3219cb005d6746461ecce94f
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-10-24 18:52:06 -0500

    refactor: Minor improvements in dialog to move files

--- src/screens/dialogs/move-file-dialog.tsx
+++ src/screens/dialogs/move-file-dialog.tsx
@@ -46,32 +46,28 @@ export const MoveFileDialog = () => {
   const handleMove = async () => {
     if (moving || !selectedFolder || !selectedFile) return;
 
-    if (!selectedFolder.isFile) {
-      setMoving(true);
-      const { success, ...res } = await moveFileService({
-        directoryUUID: selectedFolder.uuid,
-        fileUUID: selectedFile.uuid as string,
-        token: session?.token as string
-      });
+    setMoving(true);
+    const { success, ...res } = await moveFileService({
+      directoryUUID: selectedFolder.uuid,
+      fileUUID: selectedFile.uuid as string,
+      token: session?.token as string
+    });
 
-      if (!success) {
-        toast.error(res.msg);
-        setMoving(false);
-        return;
-      }
-
-      const successMessage = selectedFile.isFile
-        ? "File has been moved successfully"
-        : "Folder has been moved successfully";
-
-      toast.success(successMessage);
+    if (!success) {
+      toast.error(res.msg);
       setMoving(false);
-      closeDialog(AVAILABLE_DIALOGS.MOVE_FILE);
-      setSelectedFolder(null);
-      removeFile(selectedFile.uuid);
-    } else {
-      toast.error("This action cannot be performed");
+      return;
     }
+
+    const successMessage = selectedFile.isFile
+      ? "File has been moved successfully"
+      : "Folder has been moved successfully";
+
+    toast.success(successMessage);
+    setMoving(false);
+    closeDialog(AVAILABLE_DIALOGS.MOVE_FILE);
+    setSelectedFolder(null);
+    removeFile(selectedFile.uuid);
   };
 
   const changeDirectory = (directoryUUID: string) => {
@@ -93,36 +89,36 @@ export const MoveFileDialog = () => {
       isOpen={open}
       onClose={() => {
         closeDialog(AVAILABLE_DIALOGS.MOVE_FILE);
-        setSelectedFolder(null);
-        setFolders([]);
       }}
       title="Move File"
     >
-      <div className="line-clamp-1 max-h-[300px] w-64 overflow-y-auto p-4">
+      <div className="flex max-h-[300px] w-64 flex-col gap-2 overflow-y-auto">
         {folders.map((folder) => (
           <button
             key={folder.uuid}
-            className="mb-2 line-clamp-1 w-full rounded-lg bg-blue-600 px-4 py-2 text-left"
+            className="w-full rounded-lg bg-blue-600 px-4 py-2 text-left transition-colors hover:bg-blue-700"
             onClick={() => changeDirectory(folder.uuid)}
             aria-label={`Move to ${folder.name}`}
           >
-            <span className="font-bold text-white">
+            <span className="line-clamp-1 font-bold text-white">
               <FolderPlus className="mr-2 inline" /> {folder.name}
             </span>
           </button>
         ))}
       </div>
-      <p className="text-center text-gray-500">
-        Current Folder: {selectedFolder ? selectedFolder.name : "Root"}
-      </p>
-      {selectedFolder ? (
-        <button
-          className="hover-bg-red-700 w-full rounded-lg bg-red-600 px-4 py-2 text-white"
-          onClick={handleMove}
-        >
-          Move here
-        </button>
-      ) : null}
+      <div className=" mt-4 flex flex-col gap-4">
+        <p className="text-center text-gray-500">
+          Current Folder: {selectedFolder ? selectedFolder.name : "Root"}
+        </p>
+        {selectedFolder && (
+          <button
+            className="w-full rounded-lg border border-blue-600 bg-transparent px-4 py-2 text-blue-600 transition-colors hover:bg-blue-600 hover:text-white"
+            onClick={handleMove}
+          >
+            Move here
+          </button>
+        )}
+      </div>
     </Dialog>
   );
 };
