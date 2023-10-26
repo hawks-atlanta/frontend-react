21ea83cdc7d751520d0860c7ce28df3498dfd71c
Daniel Patiño <daniel.patino23@outlook.com>
2023-10-26 10:32:25 -0500

    fix: cypress test

--- cypress/e2e/files/share-file.cy.ts
+++ cypress/e2e/files/share-file.cy.ts
@@ -1,25 +1,16 @@
 import { faker } from "@faker-js/faker";
 
-describe("Rename File Tests", () => {
+describe("Share File Tests", () => {
   const username = faker.internet.userName();
   const password = faker.internet.password({ length: 8 });
   const folderName = faker.system.commonFileName();
 
-  it("Register a test user", () => {
+  it("Tests to share a file", () => {
     // Register a test user
     cy.visit("/register");
     cy.get("input[name=username]").type(username);
     cy.get("input[name=password]").type(password);
     cy.get("input[name=confirmPassword]").type(password);
-    cy.get("button").contains("Submit").click();
-    cy.url().should("include", "/files");
-  });
-
-  it("Create a folder", () => {
-    // Login with the test user
-    cy.visit("/login");
-    cy.get("input[name=username]").type(username);
-    cy.get("input[name=password]").type(password);
     cy.get("button").contains("Submit").click();
     cy.url().should("include", "/files");
 
@@ -33,6 +24,7 @@ describe("Rename File Tests", () => {
     cy.get(`button[aria-label='Open options menu for ${folderName}']`).click();
 
     // Click the "Share" button to rename the folder
+    cy.get("button").contains("Share").should("exist");
     cy.get("button[aria-label='Share']").click();
 
     cy.get("div[role=dialog]").should("contain", "Share file");
@@ -42,5 +34,9 @@ describe("Rename File Tests", () => {
     cy.get("button:contains('Save')").click();
     // Verify that the success message is displayed
     cy.contains("File shared successfully");
+
+    // Close the dropdown
+    cy.get(`button[aria-label='Open options menu for ${folderName}']`).click();
+    cy.get("button").contains("Share").should("not.exist");
   });
 });
