239f06284386867db9d5cf01d5a54db116741352
Daniel Patiño <daniel.patino23@outlook.com>
2023-10-26 09:34:28 -0500

    fix: cypress test

--- cypress/e2e/update-password.cy.ts
+++ cypress/e2e/update-password.cy.ts
@@ -5,7 +5,7 @@ describe("Users can change their passwords", () => {
   const password = faker.internet.password({ length: 8 });
   const password2 = faker.internet.password({ length: 8 });
 
-  it("Register a test user", () => {
+  it("Update password", () => {
     // Register a test user
     cy.visit("/register");
     cy.get("input[name=username]").type(username);
@@ -13,24 +13,20 @@ describe("Users can change their passwords", () => {
     cy.get("input[name=confirmPassword]").type(password);
     cy.get("button").contains("Submit").click();
     cy.url().should("include", "/files");
-
     // Open the profile menu
     cy.get("button").contains(username).click();
 
     // Assert the modal is open
     cy.get("button").should("contain", "Update Password");
     cy.get("a").should("contain", "Logout");
-
     cy.get("button").contains("Update Password").click();
-
     // Update the password
     cy.get("input[aria-label='Current password']").type(password);
     cy.get("input[aria-label='New password']").type(password2);
-    cy.get("button").contains("Save Changes").click();
+    cy.get("button").contains("Save changes").click();
 
     // Assert the success message is shown in the toast
     cy.contains("Password updated successfully").should("be.visible");
-
     // Make the password change fail
     cy.get("button").should("contain", "Update Password");
     cy.get("a").should("contain", "Logout");
@@ -40,11 +36,10 @@ describe("Users can change their passwords", () => {
     // Update the password
     cy.get("input[aria-label='Current password']").type(password);
     cy.get("input[aria-label='New password']").type(password2);
-    cy.get("button").contains("Save Changes").click();
+    cy.get("button").contains("Save changes").click();
 
     // Assert the success message is shown in the toast
     cy.contains("Failed to update password").should("be.visible");
-
     // Close the dialog
     cy.get("button").contains("Close").click();
   });
